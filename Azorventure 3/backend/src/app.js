e2ba1fa9ecf279cb2c783d94ba6e48a290582148
andre <leonormmoliveira2@gmail.com>
2026-05-15 14:34:31 +0000

    Fix frontend dist path for single-service deploy

--- Azorventure 3/backend/src/app.js
+++ Azorventure 3/backend/src/app.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json({ limit: '20mb' }));
 
 // Serve static files from frontend dist folder
-const frontendDistPath = path.join(__dirname, '../frontend/dist');
+const frontendDistPath = path.join(__dirname, '../../frontend/dist');
 app.use(express.static(frontendDistPath));
 
 // app.use((req, res, next) => {
